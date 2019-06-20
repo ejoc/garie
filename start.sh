5c5b23a68d10df639f618dd9556a3467cc32361b
Elias Ortega <eliass_56@hotmail.com>
2019-06-20 00:36:31 -0500

    fix docker-compose.yml

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo $HOST_URL > /tmp/var
+echo $PAGESPEED_INSIGHTS_KEY > /tmp/var
 
 # install latest version of docker the lazy way
 curl -sSL https://get.docker.com | sh
@@ -8,7 +8,7 @@ curl -sSL https://get.docker.com | sh
 usermod -aG docker ubuntu
 
 # install docker-compose
-curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 # copy the dockerfile into /srv/docker 
